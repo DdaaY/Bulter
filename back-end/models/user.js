45015fabb3b0bb336bfdc3c8c39d1f20a3a1b576
shinuki262012 <233hzw@gmail.com>
2021-09-25 22:15:30 +0100

    user schema

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -4,10 +4,27 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
     
-    username: {type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, reuqired: true, minlength: 8, maxlength: 16, trim: true },
+    email: { type: String, lowercase: true, trim: true, required: true, maxlength: 150 },
+    first_name: { type: String, reuqired: true, maxlength:50 },
+    last_name: { type: String, reuqired: true, maxlength:50 },
+    gender: { type: String, trim: true },
+    dob: { type: Date, required: true },
+    phone_number: { type: String, trim: true },
     
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String },
+    zipcode: { type: String, required: true },
 
-    
+    date_created: { type:Date, required: true },
+    POI: [
+        {
+            alias: {type: String, required: true },
+            location: { type: [Number], index: { type: '2dsphere', sparse: true} },
+        }
+    ]
 
 }, {
     timestamps: true,
