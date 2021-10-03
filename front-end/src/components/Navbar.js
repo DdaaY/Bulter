0c4a34e06754a0de9f36420ac4cebe9ec66577e2
wwbbGoS <61764495+wwbbGoS@users.noreply.github.com>
2021-10-03 21:47:19 +0100

    Update Navbar.js

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -3,21 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
 
-    constructor(props){
-        super(props);
-
-        this.state = ({
-            id: this.props.id,
-        })
-        
-
-    }
-
     render() {
         return (
             <div>
                 <nav className="navbar navbar-light bg-light justify-content-between">
-                    <Link to='/home/' className ="navbar-brand">Navbar</Link>
+                    <Link to='/' className ="navbar-brand">Navbar</Link>
                     <form className ="form-inline">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
